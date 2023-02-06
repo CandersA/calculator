4346f31ac87a5bf51481f673eb93e03b4a8ba541
CandersA <123170339+CandersA@users.noreply.github.com>
2023-02-06 15:39:53 +0200

    Refactored code

--- scripts.js
+++ scripts.js
@@ -8,10 +8,93 @@ const result = document.getElementById('result');
 
 let realNumbers = [];
 let dotClicked = false;
-let switchClicked = false;
 let finalNum = '';
 let z = 1;
 let operatorIcon;
+
+function resetDot() {
+  dot.style.filter = 'brightness(100%)';
+  dotClicked = false;
+}
+
+function whenOperator(operator) {
+  operatorIcon = operator.textContent;
+  if (operatorIcon === '%') {
+    finalNum = result.textContent;
+    finalNum = parseFloat(finalNum) * 0.01;
+    finalNum = finalNum.toString();
+    result.textContent = finalNum;
+    realNumbers[realNumbers.length - 1] = finalNum;
+  } else {
+    result.textContent = '0';
+    // eslint-disable-next-line no-unused-expressions
+    realNumbers.length < 2 ? realNumbers.push(finalNum) : false;
+    finalNum = '';
+  }
+  resetDot();
+}
+
+function divide(num1, num2) {
+  return num2 !== 0 ? parseFloat(num1) / parseFloat(num2) : 'Error';
+}
+
+function operation(operator, num1, num2) {
+  if (operator === '+') {
+    finalNum = parseFloat(num1) + parseFloat(num2);
+  } else if (operator === '-') {
+    finalNum = parseFloat(num1) - parseFloat(num2);
+  } else if (operator === 'x') {
+    finalNum = parseFloat(num1) * parseFloat(num2);
+  } else if (operator === '/') {
+    finalNum = divide(num1, num2);
+  }
+  result.textContent = Math.round(finalNum * 1000) / 1000;
+  z += 2;
+  realNumbers.push(finalNum.toString());
+  finalNum = '';
+}
+
+function whenEquals() {
+  realNumbers.push(finalNum);
+  finalNum = '';
+  operation(operatorIcon, realNumbers[z - 1], realNumbers[z]);
+  resetDot();
+}
+
+function plusMinus() {
+  if (result.textContent === '0') {
+    finalNum += '-';
+    result.textContent = '-';
+  } else {
+    let numberToSwitch = result.textContent;
+    numberToSwitch = parseFloat(numberToSwitch) * -1;
+    result.textContent = numberToSwitch;
+    realNumbers[realNumbers.length - 1] = numberToSwitch.toString();
+  }
+}
+
+function addDot() {
+  if (dotClicked === true) {
+    return;
+  }
+  finalNum += dot.textContent;
+  result.textContent = finalNum;
+  dot.style.filter = 'brightness(70%)';
+  dotClicked = true;
+}
+
+function getNumber(numbtn) {
+  finalNum += numbtn.textContent;
+  result.textContent = finalNum;
+}
+
+function clear() {
+  result.textContent = '0';
+  finalNum = '';
+  realNumbers = [];
+  z = 1;
+  resetDot();
+}
 
 dot.addEventListener('click', addDot);
 
@@ -21,105 +104,14 @@ equals.addEventListener('click', whenEquals);
 
 reset.addEventListener('click', clear);
 
-numberbtns.forEach(numbtn => {
-    numbtn.addEventListener('click', () => {
-        getNumber(numbtn);
-    });
+numberbtns.forEach((numbtn) => {
+  numbtn.addEventListener('click', () => {
+    getNumber(numbtn);
+  });
 });
 
-operators.forEach(operator => {
-    operator.addEventListener('click', () => {
-        whenOperator(operator);
-    });
+operators.forEach((operator) => {
+  operator.addEventListener('click', () => {
+    whenOperator(operator);
+  });
 });
-
-function whenOperator(operator) {
-    operatorIcon = operator.textContent;
-    if (operatorIcon == '%') {
-        finalNum = result.textContent;
-        finalNum = parseFloat(finalNum) * 0.01;
-        finalNum = finalNum.toString();
-        result.textContent = finalNum;
-        realNumbers[realNumbers.length - 1] = finalNum;
-    } else {
-        result.textContent = '0';
-        realNumbers.length < 2 ? realNumbers.push(finalNum) : false;
-        finalNum = '';
-    }
-    resetDot();
-}
-
-function whenEquals() {
-    realNumbers.push(finalNum);
-    finalNum = '';
-    operation(operatorIcon, realNumbers[z - 1], realNumbers[z]);
-    resetDot();
-};
-
-function operation(operator, num1, num2) {
-    if (operator == '+') {
-        finalNum = parseFloat(num1) + parseFloat(num2);
-    }
-    else if (operator == '-') {
-        finalNum = parseFloat(num1) - parseFloat(num2);
-    }
-    else if (operator == 'x') {
-        finalNum = parseFloat(num1) * parseFloat(num2);
-    }
-    else if (operator == '/') {
-        finalNum = divide(num1, num2);
-    }
-    result.textContent = Math.round(finalNum*1000)/1000;
-    z += 2;
-    realNumbers.push(finalNum.toString());
-    finalNum = '';
-};
-
-function plusMinus() {
-    if (result.textContent == '0') {
-        finalNum += '-';
-        result.textContent = '-';
-    } else {
-        numberToSwitch = result.textContent;
-        numberToSwitch = parseFloat(numberToSwitch) * -1; 
-        result.textContent = numberToSwitch;
-        realNumbers[realNumbers.length - 1] = numberToSwitch.toString();
-    }
-};
-
-function addDot() {
-    if (dotClicked == true) {
-        return;
-    }
-    finalNum += dot.textContent;
-    result.textContent = finalNum;
-    dot.style.filter = 'brightness(70%)';
-    dotClicked = true;
-};
-
-function resetDot() {
-    dot.style.filter = 'brightness(100%)';
-    dotClicked = false;
-}
-
-function resetSwitch() {
-    switchPlusMinus.style.filter = 'brightness(100%)';
-    switchClicked = false;
-}
-
-function getNumber(numbtn) {
-    finalNum += numbtn.textContent;
-    result.textContent = finalNum;
-};
-
-function clear() {
-    result.textContent = '0';
-    finalNum = '';
-    realNumbers = [];
-    z = 1;
-    resetDot();
-};
-
-function divide(num1,num2) {
-    return num2 != 0 ? parseFloat(num1) / parseFloat(num2) : "Error";
-};
